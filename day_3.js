383674a11557e56a3da10ed378b8f0f6a75730bf
mrjln <marjolein_tromp@hotmail.com>
2017-12-23 16:12:12 +0100

    minor changes

---
+++ day_3.js
@@ -0,0 +1,21 @@
+function manhattan(position){
+var spiral  = []
+//een spiraal heeft 8 posities of een veelvoud daarvan
+while(spiral.length != position){
+
+  for(var i = 0; i < position; i++){
+
+    
+  }
+
+
+}
+
+
+  console.log()
+}
+
+
+console.log(manhattan(23))
+
+//368078
